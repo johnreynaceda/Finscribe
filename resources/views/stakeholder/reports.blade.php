2484666ece2c24a4d149a85920b22428a5fc149c
johnreynaceda <johnreynaceda@sksu.edu.ph>
2024-06-11 19:38:03 +0800

    updats. sidebar

--- resources/views/stakeholder/reports.blade.php
+++ resources/views/stakeholder/reports.blade.php
@@ -2,11 +2,12 @@
     <div>
         <header class="text-2xl font-bold text-gray-800">REPORTS</header>
         <div class="mt-10">
-            @if (auth()->user()->user_type != 'Employee')
+            {{-- @if (auth()->user()->user_type != 'Employee')
                 <livewire:reports />
             @else
-                <h1>coming soon...</h1>
-            @endif
+
+            @endif --}}
+            <h1>coming soon...</h1>
             <script>
                 function printOut(data) {
                     var mywindow = window.open('', '', 'height=1000,width=1000');
